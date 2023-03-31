568dfb5b3cf826a72bcec74d7ae3ed255a005497
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-04-01 00:35:43 +0400

    feat: format footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,26 +1,32 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <div className="container footerContainer">
-            <div className={styles.footerSection + " " + styles.footerLogo}>
-                {/* <div className={styles.footerLogo}> */}
-                    <a href="#">EXQUISITE</a>
-                {/* </div> */}
-            </div>
-            <div className={styles.footerSection}>
-                  <a className={styles.footerItem} href="#">Личный кабинет</a>
-                  <a className={styles.footerItem} href="#">Корзина</a>
-                  <a className={styles.footerItem} href="#">Избранное</a>
-                  <a className={styles.footerItem} href="#">Контакты</a>
-                
-            </div>
-            <div className={styles.footerSection}>
-            <button className={styles.btn}>Каталог</button>
-            </div>
+      <div className="container footerContainer">
+        <div className={styles.footerSection + " " + styles.footerLogo}>
+          {/* <div className={styles.footerLogo}> */}
+          <a href="#">EXQUISITE</a>
+          {/* </div> */}
         </div>
-        
+        <div className={styles.footerSection}>
+          <a className={styles.footerItem} href="#">
+            Личный кабинет
+          </a>
+          <a className={styles.footerItem} href="#">
+            Корзина
+          </a>
+          <a className={styles.footerItem} href="#">
+            Избранное
+          </a>
+          <a className={styles.footerItem} href="#">
+            Контакты
+          </a>
+        </div>
+        <div className={styles.footerSection}>
+          <button className={styles.btn}>Каталог</button>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
