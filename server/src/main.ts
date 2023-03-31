6768750e2e1883ceae587c6e07fa3987b2af0455
droma0426 <droma0426@gmail.com>
2023-04-01 00:24:00 +0400

    feat: add start function

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
 config();
 
-
 const app = express();
-const PORT = process.env.PORT ?? 4000
+const PORT = process.env.PORT ?? 4000;
 
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`) ) 
+async function start() {
+  try {
+    app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
