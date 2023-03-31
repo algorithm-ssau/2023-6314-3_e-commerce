c3a55b65252167d8696998a6dadfdea863ecbaa8
droma0426 <droma0426@gmail.com>
2023-04-01 00:09:44 +0400

    feat: make server listen on port 4000

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,3 +2,9 @@ import express from 'express';
 import {config} from 'dotenv';
 
 config();
+
+
+const app = express();
+const PORT = process.env.PORT ?? 4000
+
+app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`) ) 
