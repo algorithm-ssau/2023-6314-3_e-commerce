895b452dd74ae4286a478b129d407093a9109fc1
Ruslan <Ruslan.ilin.2002@yandex.ru>
2023-04-18 00:34:02 +0400

    feat: instal python-dotenv and connect to DB

--- python-api/main.py
+++ python-api/main.py
@@ -1,3 +1,24 @@
+import os
+import dotenv
+import pymysql
+
+dotenv.load_dotenv('config.env')
+
+# Подключение к БД
+try:
+    connection = pymysql.connect(
+        host = os.environ['host'],
+        user = os.environ['user'],
+        password = os.environ['password'],
+        db = os.environ['db'],
+        port = int(os.environ['port']), 
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    print("Succesfull")
+except:
+    print("Сonnection is not established")
+
+# Тестовые списки для создания матрицы алгоритма подбора рекомендаций
 list_cart = [1,3,4,8090,45]
 list_favorites = [3,6,8090,2,6,56,222]
 list_recently = [4,8090,2,67,5,666]
@@ -11,6 +32,7 @@ def checkInMatr(id):
         if i[0] == id:
             check = True
     return check
+
 # Метод получения индекса id в matr
 def getIndex(id):
     index = -1
@@ -20,18 +42,20 @@ def getIndex(id):
             index = j
         j += 1
     return index
-# Метод для обработки входящих списков "избранное", "корзина", т.д.
+
+# Метод для обработки входящих списков
 def insertIntoMatr(list, weight):
-    # идём по списку
+    # Идём по списку
     for i in list:
-        # если товар с текущим id уже есть в matr
+        # Если товар с текущим id уже есть в matr,
         # находим индекс id в matr, добавляем id необходимый вес
         if checkInMatr(i) == True:
             index = getIndex(i)
             matr[index][1] += weight
-        # иначе добавляем элемент в matr,устанавливаем вес
+        # иначе добавляем элемент в matr, устанавливаем вес
         else:
             matr.append([i,weight])
+
 # Формируем matr
 if len(list_cart) != 0:
     insertIntoMatr(list_cart, 1)
@@ -42,4 +66,8 @@ if len(list_recently) != 0:
 if len(list_bought) != 0:
     insertIntoMatr(list_bought, 1)
 
+# Вывод полученой матрицы
 print(matr)
+
+# Закрываем соединение с БД
+connection.close()
