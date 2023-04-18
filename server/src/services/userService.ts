86693d24395fa0105b10facf61ac61bda247b865
droma0426 <droma0426@gmail.com>
2023-04-18 23:50:55 +0400

    feat: add basic user service

---
+++ server/src/services/userService.ts
@@ -0,0 +1,20 @@
+
+class UserService {
+    register () {
+        return 'register';
+    }
+    login () {
+        return 'login';
+    }
+    loginAdmin () {
+        return 'loginAdmin';
+    }
+    update () {
+        return 'update';
+    }
+    delete () {
+        return 'delete';
+    }
+}
+
+export const userService = new UserService();
